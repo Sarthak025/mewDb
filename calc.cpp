8b8435b3cbec002679b6f36d7a51c8575f268750
Sarthak <sarthak.srivastava@moonshinetechnology.com>
2026-06-15 04:14:08 +0530

    PRAC: Made a cli calculator using sstream

---
+++ calc.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+void startRepl() {
+	std::string expression;
+	while (true) {
+		std::cout << "mew> ";
+		getline(std::cin, expression);
+
+		// exit condition
+		if (expression == "exit" || expression == "quit")
+			break;
+
+		if (expression == "")
+			continue;
+
+		std::stringstream ss;
+		ss.str(expression);
+
+		int a, b;
+		char op;
+		std::string extra;
+
+		if (!(ss >> a >> op >> b) || (ss >> extra)) {
+			std::cout << "ERROR: unknown command" << std::endl;
+			continue;
+		}
+
+		int res;
+		if (op == '+') {
+			res = a + b;
+		} else if (op == '-') {
+			res = a - b;
+		} else if (op == '*') {
+			res = a * b;
+		} else if (op == '/') {
+			if (b == 0) {
+				std::cout << "ERROR: divisor cannot be 0" << std::endl;
+				continue;
+			}
+			res = a / b;
+		} else if (op == '%') {
+			if (b == 0) {
+				std::cout << "ERROR: modulus cannot be 0" << std::endl;
+				continue;
+			}
+			res = a % b;
+		} else {
+			std::cout << "ERROR: unknown command" << std::endl;
+			continue;
+		}
+
+		std::cout << "= " << res << std::endl;
+	}
+}
+
+int main() {
+	startRepl();
+	return 0;
+}
