7e28a122d92e6c855ebc3a6df7f654056d2ce464
Sarthak025 <sarthaksrivastava025@gmail.com>
2026-09-01 02:26:24 +0530

    FEAT: Manifest format and class updates

--- include/constants.h
+++ include/constants.h
@@ -9,10 +9,15 @@ constexpr uint8_t SS_TABLE_VERSION = 1;
 constexpr uint32_t WAL_MAGIC_NUMBER = 0xDEADBEEF;
 constexpr uint32_t SS_TABLE_MAGIC_NUMBER = 0xDEADBEEF;
 
-inline const std::string WAL_FILE_NAME = "wal";
-inline const std::string SS_TABLE_NAME = "ss_table";
+inline const std::string WAL_FILE_NAME = "data/wal";
+inline const std::string SS_TABLE_NAME = "data/ss_table";
 
 constexpr uint8_t SET = 0;
 constexpr uint8_t DELETE = 1;
 
 constexpr uint64_t MEM_TABLE_SIZE_LIMIT = 1e3;
+
+
+constexpr uint32_t MANIFEST_MAGIC_NUMBER = 0xDEADBEEF;
+constexpr uint8_t MANIFEST_VERSION = 1;
+inline const std::string MANIFEST_FILE_NAME = "data/manifest.txt";
--- include/manifest.h
+++ include/manifest.h
@@ -3,12 +3,24 @@
 #include <cstdint>
 #include <vector>
 #include <string>
+#include <fstream>
 
+// Format for manifest file
+// 
+// MANIFEST_MAGIC_NUMBER
+// MANIFEST_VERSION
+// NEXT_SS_TABLE_INDEX
+// NUM_SS_TABLES
+// SS_TABLE_INDEX_1
+// SS_TABLE_INDEX_2
+// ......
 
 class manifest {
 private:
     std::string manifest_file_name;
-    uint64_t next_ss_table_index;
+    std::fstream manifest_file;
+    uint64_t next_ss_table_index = 0;
+    uint64_t num_ss_tables = 0;
     std::vector<uint64_t> available_ss_table_indices;
 
 public:
